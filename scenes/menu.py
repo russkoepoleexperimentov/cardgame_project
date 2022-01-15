eec3f647f79d2e7ff669433bd9944be61f5ee16c
Матвей <kuzmatkem@yandex.ru>
2022-01-15 20:49:56 +0700

    added 'decks' scene (early test version)

--- scenes/menu.py
+++ scenes/menu.py
@@ -9,24 +9,12 @@ from core.application import close as close_app
 from core.components.drag_handler import DragHandler
 from core.components.drop_handler import DropHandler
 from core.localization import translate_string
-from core import config
+from core import config, scene_manager
 
 from game import cursor
 from game.button_sounds import ButtonSounds
 from game.contstants import BUTTON_DEFAULT_DESIGN, BUTTONS_SIZE, BUTTONS_TOP_OFFSET
 from game import alert_popup
-
-
-import asyncio
-import threading
-
-
-async def wait_des():
-    await alert_popup.instance().await_for_decision('agasgas')
-
-
-def do_smth():
-    threading.Thread(target=asyncio.run(wait_des())).start()
 
 
 class MenuScene(Scene):
@@ -38,7 +26,8 @@ class MenuScene(Scene):
 
         screen_w, screen_h = tuple(map(int, config.get_value('vid_mode').split('x')))
         screen = Vector(screen_w, screen_h)
-        buttons_layout_group = VerticalLayoutGroup(position=Vector(screen_w // 2 - BUTTONS_SIZE.x // 2,
+        buttons_layout_group = VerticalLayoutGroup(position=Vector(screen_w // 2 -
+                                                                   BUTTONS_SIZE.x // 2,
                                                                    BUTTONS_TOP_OFFSET), spacing=10)
         buttons_layout_group.draw_bounds = True
         self.add_game_object(buttons_layout_group)
@@ -46,20 +35,29 @@ class MenuScene(Scene):
         background = Image(size=screen, sprite=load_image('sprites/ui/menu.png'))
         self.add_game_object(background, -100)
 
-        game_title = Text(size=BUTTONS_SIZE, title=translate_string('game_name'), align='center', valign='middle')
+        game_title = Text(size=BUTTONS_SIZE, title=translate_string('game_name'), align='center',
+                          valign='middle')
         game_title.set_parent(buttons_layout_group)
 
-        start_button = Button(**BUTTON_DEFAULT_DESIGN, size=BUTTONS_SIZE, title=translate_string('ui.start'))
+        start_button = Button(**BUTTON_DEFAULT_DESIGN, size=BUTTONS_SIZE,
+                              title=translate_string('ui.start'))
         start_button.set_parent(buttons_layout_group)
         start_button.add_component(ButtonSounds)
-        start_button.on_click.add_listener(do_smth)
 
-        settings_button = Button(**BUTTON_DEFAULT_DESIGN, size=BUTTONS_SIZE, title=translate_string('ui.settings'))
+        decks_button = Button(**BUTTON_DEFAULT_DESIGN, size=BUTTONS_SIZE,
+                              title=translate_string('ui.decks'))
+        decks_button.set_parent(buttons_layout_group)
+        decks_button.add_component(ButtonSounds)
+        decks_button.on_click.add_listener(self.load_decks_showroom)
+
+        settings_button = Button(**BUTTON_DEFAULT_DESIGN, size=BUTTONS_SIZE,
+                                 title=translate_string('ui.settings'))
         settings_button.set_parent(buttons_layout_group)
         settings_button.add_component(ButtonSounds)
         settings_button.interactable = False
 
-        exit_button = Button(**BUTTON_DEFAULT_DESIGN, size=BUTTONS_SIZE, title=translate_string('ui.quit'))
+        exit_button = Button(**BUTTON_DEFAULT_DESIGN, size=BUTTONS_SIZE,
+                             title=translate_string('ui.quit'))
         exit_button.set_parent(buttons_layout_group)
         exit_button.add_component(ButtonSounds)
         exit_button.on_click.add_listener(close_app)
@@ -71,3 +69,12 @@ class MenuScene(Scene):
         # position=Vector(50, 50), sprite=load_image('sprites/ui/button.png'))
         # test_drag_go.add_component(DragHandler)
         # self.add_game_object(test_drag_go)
+
+    def load_decks_showroom(self):
+        # temporary
+        scene = Scene()
+        label = Text(size=Vector(200, 100), title='калоды')
+        scene.add_game_object(label)
+        # end
+
+        scene_manager.load(scene)
