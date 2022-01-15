5d9d814de6c989112e9b6aff38f766fcaeae37ca
Матвей <kuzmatkem@yandex.ru>
2022-01-15 17:39:31 +0700

    updated .gitignore

--- scenes/menu.py
+++ scenes/menu.py
@@ -16,7 +16,18 @@ from game.button_sounds import ButtonSounds
 from game.contstants import BUTTON_DEFAULT_DESIGN, BUTTONS_SIZE, BUTTONS_TOP_OFFSET
 from game import alert_popup
 
+
 import asyncio
+import threading
+
+
+async def wait_des():
+    await alert_popup.instance().await_for_decision('agasgas')
+
+
+def do_smth():
+    threading.Thread(target=asyncio.run(wait_des())).start()
+
 
 class MenuScene(Scene):
     def __init__(self):
@@ -37,11 +48,11 @@ class MenuScene(Scene):
 
         game_title = Text(size=BUTTONS_SIZE, title=translate_string('game_name'), align='center', valign='middle')
         game_title.set_parent(buttons_layout_group)
-        self.add_game_object(game_title)
 
         start_button = Button(**BUTTON_DEFAULT_DESIGN, size=BUTTONS_SIZE, title=translate_string('ui.start'))
         start_button.set_parent(buttons_layout_group)
         start_button.add_component(ButtonSounds)
+        start_button.on_click.add_listener(do_smth)
 
         settings_button = Button(**BUTTON_DEFAULT_DESIGN, size=BUTTONS_SIZE, title=translate_string('ui.settings'))
         settings_button.set_parent(buttons_layout_group)
@@ -53,9 +64,10 @@ class MenuScene(Scene):
         exit_button.add_component(ButtonSounds)
         exit_button.on_click.add_listener(close_app)
 
-        drop_handle = exit_button.add_component(DropHandler)
-        drop_handle.on_drop.add_listener(lambda drag: print(drag))
+        # drop_handle = exit_button.add_component(DropHandler)
+        # drop_handle.on_drop.add_listener(lambda drag: print(drag))
 
-        test_drag_go = Image(size=Vector(100, 100), position=Vector(50, 50), sprite=load_image('sprites/ui/button.png'))
-        test_drag_go.add_component(DragHandler)
-        self.add_game_object(test_drag_go)
+        # test_drag_go = Image(size=Vector(100, 100),
+        # position=Vector(50, 50), sprite=load_image('sprites/ui/button.png'))
+        # test_drag_go.add_component(DragHandler)
+        # self.add_game_object(test_drag_go)
