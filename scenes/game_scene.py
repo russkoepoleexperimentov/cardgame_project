b85edb29228eaeca2a4cbbb1ee4c8fc0f7e09ef8
kuryga <kuryshkin05@gmail.com>
2022-01-21 20:13:40 +0700

    game 0.2

--- scenes/game_scene.py
+++ scenes/game_scene.py
@@ -1,6 +1,6 @@
 import sqlite3
 from core import config
-from game.contstants import PLAYER_STATS_BASE, BUTTON_DEFAULT_DESIGN, BUTTONS_SIZE
+from game.contstants import PLAYER_STATS_BASE, BUTTON_DEFAULT_DESIGN
 from core.resources import load_image
 from core.scene import Scene
 from core.ui.button import Button
@@ -14,8 +14,7 @@ from core import scene_manager
 from scenes.menu import MenuScene
 from random import sample
 from core.ui.layout_group import HorizontalLayoutGroup
-from core.components.drag_handler import DragHandler
-from core.components.drop_handler import DropHandler
+from game.game_scene.game_card import GameCard
 
 
 class GameScene(Scene):
@@ -35,7 +34,7 @@ class GameScene(Scene):
         self.add_game_object(self.back_btn)
 
         self.choice_label = Text(title=translate_string('ui.deck_choice'), align='center',
-                            valign='middle', size=Vector(400, 30), position=Vector(483, 30))
+                                 valign='middle', size=Vector(400, 30), position=Vector(483, 30))
         self.add_game_object(self.choice_label)
 
         self.soviet_btn = Button(size=Vector(350, 175),
@@ -46,12 +45,12 @@ class GameScene(Scene):
         self.add_game_object(self.soviet_btn)
 
         self.soviet_label = Text(title=translate_string('soviet'), align='center', valign='middle',
-                            size=Vector(350, 30), position=Vector(222, 450))
+                                 size=Vector(350, 30), position=Vector(222, 450))
         self.add_game_object(self.soviet_label)
 
         self.germany_btn = Button(size=Vector(350, 175),
-                             sprite=load_image('sprites/ui/germany_flag.png'),
-                             position=Vector(774, 250))
+                                  sprite=load_image('sprites/ui/germany_flag.png'),
+                                  position=Vector(774, 250))
         self.germany_btn.add_component(ButtonSounds)
         self.germany_btn.on_click.add_listener(self.germany_choice)
         self.add_game_object(self.germany_btn)
@@ -80,6 +79,7 @@ class GameScene(Scene):
 
         self.my_cards_count = 15
         self.my_ammo = 0
+        self.turn_num = 0
 
         self.my_fuel = 0
         background_image = load_image('sprites/ui/my_deck_background.jpg')
@@ -89,89 +89,94 @@ class GameScene(Scene):
         line_box_size = Vector(566, 141.6)
         self.card_size = Vector(99.12, 141.6)
         backside_image = load_image('sprites/card_face_back.png')
-        deck_box_size = Vector(375, 141.6)
+        deck_box_size = Vector(456.88, 141.6)
 
-        enemy_backside_1 = Image(position=Vector(409.4, -80.8), size=self.card_size,
+        enemy_backside_1 = Image(position=Vector(400, -80.8), size=self.card_size,
                                  sprite=backside_image)
         self.add_game_object(enemy_backside_1)
 
-        enemy_backside_2 = Image(position=Vector(412.4, -77.8), size=self.card_size,
+        enemy_backside_2 = Image(position=Vector(403, -77.8), size=self.card_size,
                                  sprite=backside_image)
         self.add_game_object(enemy_backside_2)
 
-        enemy_backside_3 = Image(position=Vector(415.4, -74.8), size=self.card_size,
+        enemy_backside_3 = Image(position=Vector(406, -74.8), size=self.card_size,
                                  sprite=backside_image)
         self.add_game_object(enemy_backside_3)
 
-        self.enemy_deck_box = Image(position=Vector(495, -80.8), size=deck_box_size)
+        self.enemy_deck_box = Image(position=Vector(509.12, -80.8), size=deck_box_size)
         self.enemy_deck_box.add_component(HorizontalLayoutGroup)
         self.add_game_object(self.enemy_deck_box)
 
         self.enemy_backline_box = Image(position=Vector(400, 70.8), size=line_box_size,
-                                      sprite=background_image)
+                                        sprite=background_image)
         self.enemy_backline_box.add_component(HorizontalLayoutGroup)
         self.add_game_object(self.enemy_backline_box)
 
-        self.enemy_backline_icon = Image(position=Vector(976, 116.6), size=icon_size,
-                                         sprite=backline_icon)
-        self.add_game_object(self.enemy_backline_icon)
+        enemy_backline_icon = Image(position=Vector(976, 116.6), size=icon_size,
+                                    sprite=backline_icon)
+        self.add_game_object(enemy_backline_icon)
 
         self.enemy_frontline_box = Image(position=Vector(400, 222.4), size=line_box_size,
                                          sprite=background_image)
         self.enemy_frontline_box.add_component(HorizontalLayoutGroup)
         self.add_game_object(self.enemy_frontline_box)
 
-        self.enemy_fronline_icon = Image(position=Vector(976, 268.2), size=icon_size,
-                                         sprite=frontline_icon)
-        self.add_game_object(self.enemy_fronline_icon)
+        enemy_frontline_icon = Image(position=Vector(976, 268.2), size=icon_size,
+                                     sprite=frontline_icon)
+        self.add_game_object(enemy_frontline_icon)
 
-        self.black_line = Image(position=Vector(0, 369), size=Vector(1366, 10),
-                                sprite=load_image('sprites/ui/black_line.jpg'))
-        self.add_game_object(self.black_line)
+        black_line = Image(position=Vector(0, 369), size=Vector(1366, 10),
+                           sprite=load_image('sprites/ui/black_line.jpg'))
+        self.add_game_object(black_line)
 
         self.my_frontline_box = Image(position=Vector(400, 384), size=line_box_size,
                                       sprite=background_image)
         self.my_frontline_box.add_component(HorizontalLayoutGroup)
         self.add_game_object(self.my_frontline_box)
 
-        self.my_frontline_icon = Image(position=Vector(976, 429.8), size=icon_size,
-                                       sprite=frontline_icon)
-        self.add_game_object(self.my_frontline_icon)
+        my_frontline_icon = Image(position=Vector(976, 429.8), size=icon_size,
+                                  sprite=frontline_icon)
+        self.add_game_object(my_frontline_icon)
 
         self.my_backline_box = Image(position=Vector(400, 535.6), size=line_box_size,
                                      sprite=background_image)
         self.my_backline_box.add_component(HorizontalLayoutGroup)
         self.add_game_object(self.my_backline_box)
 
-        self.my_backline_icon = Image(position=Vector(976, 581.4), size=icon_size,
-                                      sprite=backline_icon)
-        self.add_game_object(self.my_backline_icon)
+        my_backline_icon = Image(position=Vector(976, 581.4), size=icon_size, sprite=backline_icon)
+        self.add_game_object(my_backline_icon)
 
-        my_backside = Image(position=Vector(385.88, 687.2), size=self.card_size,
-                              sprite=backside_image)
+        my_backside = Image(position=Vector(400, 687.2), size=self.card_size,
+                            sprite=backside_image)
         self.add_game_object(my_backside)
 
-        self.my_deck_box = Image(position=Vector(495, 687.2), size=deck_box_size)
+        self.my_deck_box = Image(position=Vector(509.12, 687.2), size=deck_box_size)
         self.my_deck_box.add_component(HorizontalLayoutGroup)
         self.add_game_object(self.my_deck_box)
 
-        self.ammo_icon = Image(position=Vector(1200, 419), size=icon_size,
-                               sprite=load_image('sprites/ui/ammo.png'))
-        self.add_game_object(self.ammo_icon)
+        self.end_turn_btn = Button(**BUTTON_DEFAULT_DESIGN, position=Vector(1200, 394),
+                                   size=Vector(120, 50), title=translate_string('end_turn'))
+        self.end_turn_btn.label.set_font_size(20)
+        self.end_turn_btn.on_click.add_listener(self.end_turn)
+        self.add_game_object(self.end_turn_btn)
 
-        self.ammo_info = Text(position=Vector(1270, 419), size=icon_size, valign='middle',
+        ammo_icon = Image(position=Vector(1200, 444), size=icon_size,
+                               sprite=load_image('sprites/ui/ammo.png'))
+        self.add_game_object(ammo_icon)
+
+        self.ammo_info = Text(position=Vector(1270, 444), size=icon_size, valign='middle',
                               title=str(self.my_ammo))
         self.add_game_object(self.ammo_info)
 
-        self.fuel_icon = Image(position=Vector(1200, 529), size=icon_size,
+        fuel_icon = Image(position=Vector(1200, 504), size=icon_size,
                                sprite=load_image('sprites/ui/fuel.png'))
-        self.add_game_object(self.fuel_icon)
+        self.add_game_object(fuel_icon)
 
-        self.fuel_info = Text(position=Vector(1270, 529), size=icon_size, valign='middle',
+        self.fuel_info = Text(position=Vector(1270, 504), size=icon_size, valign='middle',
                               title=str(self.my_fuel))
         self.add_game_object(self.fuel_info)
 
-        self.card_count_info = Text(position=Vector(385.88, 687.2), size=Vector(99.12, 80.8),
+        self.card_count_info = Text(position=Vector(400, 687.2), size=Vector(99.12, 80.8),
                                     title=str(self.my_cards_count), align='center', valign='middle')
         self.add_game_object(self.card_count_info)
 
@@ -180,17 +185,24 @@ class GameScene(Scene):
     def my_turn(self):
         self.my_cards_count -= 3
         self.card_count_info.set_title(str(self.my_cards_count))
-        self.my_ammo += 3
-        self.ammo_info.set_title(str(self.my_ammo))
-        self.my_fuel += 1
-        self.fuel_info.set_title(str(self.my_fuel))
+        self.turn_num += 1
+        if self.turn_num <= 3:
+            self.my_ammo += 1
+            self.ammo_info.set_title(str(self.my_ammo))
+        else:
+            self.my_ammo += 2
+            self.ammo_info.set_title(str(self.my_ammo))
+            self.my_fuel += 1
+            self.fuel_info.set_title(str(self.my_fuel))
         new_cards = sample(self.my_deck, k=3)
         for card_info in new_cards:
             card = card_info.build_card_object(card_width=self.card_size.x)
-            card.add_component(DragHandler)
-            card.add_component(DropHandler)
+            game_card = card.add_component(GameCard)
+            game_card.init(card_info)
             card.set_parent(self.my_deck_box)
 
+    def end_turn(self):
+        pass
 
     def back(self):
         scene_manager.load(MenuScene())
